f70080b3c07c44088061f6cc54ee669a0c808884
Muhamad Nur Syami <nursami385@gmail.com>
2023-09-21 11:09:47 +0700

    second commit : perbaharui dom, add button, fix form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Tambahkan kode JavaScript kalian di file ini
 // Tentang Kami
 const tentangKami = document.querySelector(".tentangKami");
 const detailTentangKami = document.querySelector("#detailtentangkami");
@@ -23,3 +24,82 @@ programKami.addEventListener("click", function () {
     detailProgramKami.style.display = "none";
   }
 });
+
+// FORM
+// AMBIL NILAI DARI DOM
+
+function handleGetFormData () {
+   const name = document.getElementById("name").value;
+   const email = document.getElementById("email").value;
+   const city = document.getElementById("city").value;
+   const zipCode = document.getElementById("zip-code").value;
+   const status = document.getElementById("status").checked;
+
+  return {
+    name, email, city, zipCode, status
+  }
+}
+
+function isNumber(inputString){
+  return !isNaN(inputString);
+ 
+}
+
+
+  
+function checkboxIsChecked (){
+  if (handleGetFormData().status == true){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+const data = handleGetFormData();
+
+function validateFormData(data) {
+  if (data !== null && isNumber(data.zipCode) && checkboxIsChecked() ) {
+  
+    return true;
+  } else {
+    
+    return false;
+  }
+}
+
+const warning = document.getElementById("warning");
+function submit (){
+  if(validateFormData(data) == false){
+    warning.innerHTML = "Periksa form anda sekali lagi";
+  }
+  else{
+     warning.innerHTML = "";
+  }
+
+}
+
+const submitForm = document.getElementById("submit-form");
+submitForm.addEventListener("click", function(event){
+  event.preventDefault();
+  submit();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
